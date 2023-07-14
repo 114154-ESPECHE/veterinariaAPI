70e0ae3090d4fa1d71a81385f1d9c1403ee808e0
114154-ESPECHE <114154@tecnicatura.frc.utn.edu.ar>
2023-07-14 18:32:32 -0300

    front buscarUsuarioDni funciona y trae los datos

--- src/main/java/com/example/veterinariaapi/Controllers/UsuarioController.java
+++ src/main/java/com/example/veterinariaapi/Controllers/UsuarioController.java
@@ -16,6 +16,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
